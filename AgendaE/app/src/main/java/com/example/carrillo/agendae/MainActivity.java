4b37255838fc29f36c9decb25bdfd0d4bc2beb69
Karrillo <Darkvort3x@gmail.com>
2016-11-13 13:32:15 -0600

    Actualizacion

--- AgendaE/app/src/main/java/com/example/carrillo/agendae/MainActivity.java
+++ AgendaE/app/src/main/java/com/example/carrillo/agendae/MainActivity.java
@@ -1,9 +1,28 @@
 package com.example.carrillo.agendae;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DataBaseManager manager;
+    //Componentes
+    private ListView lista;
+    private EditText buscar;
+    private ImageButton ingresar;
+    private Button salir;
+    private TextWatcher text = null;
+    //lista para llenar la listView
+    private List<Notas> notas;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
