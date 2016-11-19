f319ed8e21b013b071ef1f37e4f7bedc8d987a3b
Karrillo <Darkvort3x@gmail.com>
2016-11-19 13:33:09 -0600

    Actualizacion

--- AgendaE/app/src/main/java/com/example/carrillo/agendae/MainActivity.java
+++ AgendaE/app/src/main/java/com/example/carrillo/agendae/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -28,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**Instacia de la clase DataBaseManager**/
+        manager = new DataBaseManager(this);
+        /**Componentes**/
+        lista = (ListView) findViewById(R.id.lista_tareas);
+        buscar = (EditText) findViewById(R.id.txt_buscar);
+        ingresar = (ImageButton) findViewById(R.id.btn_insertar);
+        salir = (Button) findViewById(R.id.btn_salir);
+        /**base de datos para el listView**/
+        manager.open();
+        notas = manager.todasNotas();
+        refreshDisplay();
+        manager.close();
+    }
+    public void refreshDisplay() {
+        ArrayAdapter<Notas> adapter = new ArrayAdapter<Notas>(this, android.R.layout.simple_list_item_1, notas);
+        lista.setAdapter(adapter);
     }
 }
